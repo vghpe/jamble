5337fedc14caf00106bca1cb0d4ed024fe8fa3b1
Henrik <gg02hpe@gmail.com>
2025-09-11 20:46:22 +0200

    Debug menu added

--- dist/jamble.js
+++ dist/jamble.js
@@ -508,9 +508,18 @@ var Jamble;
     var proto = (typeof location !== 'undefined' ? location.protocol : '');
     var isHttp = proto === 'http:' || proto === 'https:';
     if (isHttp) {
-        Jamble.Settings.loadFrom('dist/profiles/default.json');
+        Jamble.Settings.loadFrom('dist/profiles/default.json').finally(function () {
+            try {
+                window.dispatchEvent(new CustomEvent('jamble:settingsLoaded'));
+            }
+            catch (_e) { }
+        });
     }
     else {
         Jamble.Settings.reset();
+        try {
+            window.dispatchEvent(new CustomEvent('jamble:settingsLoaded'));
+        }
+        catch (_e) { }
     }
 })();
--- src/boot.ts
+++ src/boot.ts
@@ -7,9 +7,12 @@
   var proto = (typeof location !== 'undefined' ? location.protocol : '');
   var isHttp = proto === 'http:' || proto === 'https:';
   if (isHttp) {
-    Jamble.Settings.loadFrom('dist/profiles/default.json');
+    Jamble.Settings.loadFrom('dist/profiles/default.json').finally(function(){
+      try { window.dispatchEvent(new CustomEvent('jamble:settingsLoaded')); } catch(_e){}
+    });
   } else {
     // Use embedded defaults without fetching; no console error
     Jamble.Settings.reset();
+    try { window.dispatchEvent(new CustomEvent('jamble:settingsLoaded')); } catch(_e){}
   }
 })();
